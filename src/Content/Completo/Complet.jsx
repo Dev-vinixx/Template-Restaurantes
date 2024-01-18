19fa487c28939f74a07757093c090ac4b4710aa5
Dev-vinixx <viniciuseduardo0500@gmail.com>
2024-01-18 15:47:10 -0300

    I'm going to have to stop this project for a while because I'm focused on my studies with Java and this project is about a study that I don't have at the moment.

--- src/Content/Completo/Complet.jsx
+++ src/Content/Completo/Complet.jsx
@@ -19,7 +19,31 @@ function Complet() {
     </div>
     </nav>
    </header>
-   <section className={styles.section}></section>
+   <section className={styles.section}>
+    <div className={styles.divCarrossel}>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="public\Complet\imageUm.jpg" alt="Primeiro Slide" />
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="public\Complet\imageDois.jpg" alt="Segundo Slide" />
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="public\Complet\imageTres.jpg" alt="Terceiro Slide" />
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Próximo</span>
+  </a>
+</div>
+    </div>
+   </section>
    <footer className={styles.footer}><p>feito por <span><a href="">vinixx</a></span> &copy;</p><img className={styles.imageLogo2} src="./public\Complet\logo_trial.png" alt="" /></footer>
   </main>
 
